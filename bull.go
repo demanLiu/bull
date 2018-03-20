82d0dc79be3cff4f9d6b03b5801d5566e1a725f7
liuhuan <qq384012351@gmail.com>
2018-03-21 00:25:53 +0800

    main

--- bull.go
+++ bull.go
@@ -1,12 +1,49 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"math"
+	"net/http"
+	"runtime"
+	"strings"
+	"time"
 )
 
 func main() {
-	flag.Parse()
-	fmt.Println(flag.Args())
 
+	defaultNum := runtime.NumCPU()
+	cNum := flag.Int("c", 1, "nums of client")
+	requestNum := flag.Int("n", 1, "nums of request")
+	cpuNum := flag.Int("cpu", defaultNum, "nums of cpu")
+	flag.Parse()
+	finallyCpuNum := math.Max(float64(*cpuNum), float64(defaultNum))
+	runtime.GOMAXPROCS(int(finallyCpuNum))
+	url := flag.Args()[0]
+	fmt.Println(*cNum)
+	fmt.Println(*requestNum)
+	fmt.Println(url)
+	for *cNum > 0 {
+		go httpget(url, *cNum)
+		*cNum--
+	}
+
+	time.Sleep(3 * time.Second)
+}
+func httpget(url string, clientNums int) bool {
+	if !strings.Contains(url, ":") {
+		b := bytes.Buffer{}
+		b.WriteString("http://")
+		b.WriteString(url)
+		url = b.String()
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+
+	}
+	fmt.Println(res.StatusCode)
+	return res.StatusCode == 200
 }
